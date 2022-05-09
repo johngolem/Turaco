eabf3ac971fb328322e25254864bbfe69c94e770
John Paul <johnpaul@trees.local>
2022-05-09 12:10:51 +0300

    used page controller and view port fraction to have product images overlap

--- lib/home/product_page_body.dart
+++ lib/home/product_page_body.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class ProductPageBody extends StatefulWidget {
@@ -8,11 +10,13 @@ class ProductPageBody extends StatefulWidget {
 }
 
 class _ProductPageBodyState extends State<ProductPageBody> {
+  PageController pageController = PageController(viewportFraction: 0.85);
   @override
   Widget build(BuildContext context) {
     return Container(
-      height:320,
+      height: 320,
       child: PageView.builder(
+          controller: pageController,
           itemCount: 5,
           itemBuilder: (context, position) {
             return _buildPageItem(position);
@@ -21,18 +25,29 @@ class _ProductPageBodyState extends State<ProductPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left:5,right: 5),
-      decoration: BoxDecoration(
-      borderRadius:BorderRadius.circular(35),
-      color: index.isEven? Color(0xFF69C5df):Color(0xff9294cc),
-      image: DecorationImage(
-      fit: BoxFit.cover,
-        image:AssetImage(
-          "assets/image/nike.jpeg")
-        )
-       ),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(35),
+              color: index.isEven ? Color(0xFF69C5df) : Color(0xff9294cc),
+              image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/image/nike.jpeg"))),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+              height: 140,
+              margin: EdgeInsets.only(left: 40, right: 40, bottom: 15),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(35),
+                color: Colors.white,
+              )),
+        ),
+      ],
     );
   }
 }
