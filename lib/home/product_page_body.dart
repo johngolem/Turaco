559beefbccdbb23e2132aef3c4db5410357c5be5
John Paul <johnpaul@trees.local>
2022-05-09 15:44:41 +0300

    add wrap with the star icons

--- lib/home/product_page_body.dart
+++ lib/home/product_page_body.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:turac/utils/colors.dart';
+import 'package:turac/widgets/big_text.dart';
 
 class ProductPageBody extends StatefulWidget {
   const ProductPageBody({Key? key}) : super(key: key);
@@ -41,11 +43,28 @@ class _ProductPageBodyState extends State<ProductPageBody> {
           alignment: Alignment.bottomCenter,
           child: Container(
               height: 140,
-              margin: EdgeInsets.only(left: 40, right: 40, bottom: 15),
+              margin: EdgeInsets.only(left: 25, right: 25, bottom: 15),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(35),
                 color: Colors.white,
-              )),
+              ),
+              child: Container(
+                padding: EdgeInsets.only(top: 10,left: 15,right: 15),
+                child: Column(children: [
+                  BigText(text: "Kicks Tanzania"),
+                  SizedBox(height:10,),
+                  Row(
+                    children: [
+                    Wrap(
+                      children: List.generate(5, (index) => Icon(Icons.star, color: AppColors.mainColor, size:15,))
+                      
+                    )
+                    
+                  ],
+                  )
+                ]),
+              ),
+              ),
         ),
       ],
     );
