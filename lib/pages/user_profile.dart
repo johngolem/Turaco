16f76ae6a674b7d3e2c11d560fe4ac588b7a1ed1
John Paul <johnpaul@trees.local>
2022-06-02 08:58:55 +0300

    made accounts

--- lib/pages/user_profile.dart
+++ lib/pages/user_profile.dart
@@ -21,45 +21,53 @@ class AccountProfile extends StatelessWidget {
       body: Container(
         width: double.maxFinite,
         margin: EdgeInsets.only(top: Dimensions.height20),
-        child: Column(
-          children: [
-          AppIcon(icon: Icons.person, 
-          backgroundColor: AppColors.mainColor, 
-            iconColor: Colors.white,
-            size: Dimensions.height15*10,
-            ),
-          SizedBox(height: Dimensions.height30,),
-          AccountWidget(
-            appIcon: AppIcon(icon: Icons.person,backgroundColor: AppColors.mainColor,size: Dimensions.height10*5,), 
-            bigText: BigText(text: "name of user",)
-            ),
-          SizedBox(height: Dimensions.height10,),
-          AccountWidget(
-            appIcon: AppIcon(icon: Icons.email,backgroundColor: Colors.green,size: Dimensions.height10*5,), 
-            bigText: BigText(text: "email",)
-            ),
-          SizedBox(height: Dimensions.height20,),
-          AccountWidget(
-            appIcon: AppIcon(icon: Icons.location_on,backgroundColor: Colors.pink,size: Dimensions.height10*5,), 
-            bigText: BigText(text: "Address of user",)
-            ),
-          SizedBox(height: Dimensions.height20,),
-          AccountWidget(
-            appIcon: AppIcon(icon: Icons.person,backgroundColor: Colors.red,size: Dimensions.height10*5,), 
-            bigText: BigText(text: "Type of user",)
-            ),
-          SizedBox(height: Dimensions.height20,),
-          AccountWidget(
-            appIcon: AppIcon(icon: Icons.phone,backgroundColor: Colors.yellow,size: Dimensions.height10*5,), 
-            bigText: BigText(text: "phone number",)
-            ),
-          SizedBox(height: Dimensions.height20,),
-          AccountWidget(
-            appIcon: AppIcon(icon: Icons.add_a_photo,backgroundColor: Colors.blue,size: Dimensions.height10*5,), 
-            bigText: BigText(text: "profile photo",)
-            ),
-          SizedBox(height: Dimensions.height20,),
-        ]),
+        child: Expanded(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+              AppIcon(icon: Icons.person, 
+              backgroundColor: AppColors.mainColor, 
+                iconColor: Colors.white,
+                size: Dimensions.height15*10,
+                ),
+              SizedBox(height: Dimensions.height30,),
+              Column(
+                children: [
+                  AccountWidget(
+                appIcon: AppIcon(icon: Icons.person,backgroundColor: AppColors.mainColor,size: Dimensions.height10*5,), 
+                bigText: BigText(text: "name of the user",)
+                ),
+              SizedBox(height: Dimensions.height10,),
+              AccountWidget(
+                appIcon: AppIcon(icon: Icons.email,backgroundColor: Colors.green,size: Dimensions.height10*5,), 
+                bigText: BigText(text: "email",)
+                ),
+              SizedBox(height: Dimensions.height20,),
+              AccountWidget(
+                appIcon: AppIcon(icon: Icons.location_on,backgroundColor: Colors.pink,size: Dimensions.height10*5,), 
+                bigText: BigText(text: "Address of user",)
+                ),
+              SizedBox(height: Dimensions.height20,),
+              AccountWidget(
+                appIcon: AppIcon(icon: Icons.person,backgroundColor: Colors.red,size: Dimensions.height10*5,), 
+                bigText: BigText(text: "Type of user",)
+                ),
+              SizedBox(height: Dimensions.height20,),
+              AccountWidget(
+                appIcon: AppIcon(icon: Icons.phone,backgroundColor: Colors.yellow,size: Dimensions.height10*5,), 
+                bigText: BigText(text: "phone number",)
+                ),
+              SizedBox(height: Dimensions.height20,),
+              AccountWidget(
+                appIcon: AppIcon(icon: Icons.add_a_photo,backgroundColor: Colors.blue,size: Dimensions.height10*5,), 
+                bigText: BigText(text: "profile photo",)
+                ),
+              SizedBox(height: Dimensions.height20,),
+                ],
+              )
+            ]),
+          ),
+        ),
       ),
     );
   }
