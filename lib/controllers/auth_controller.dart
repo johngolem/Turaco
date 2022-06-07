53b81a5ea9da5db61cc3e0bac5447a80a7e15d3f
John Paul <johnpaul@trees.local>
2022-06-07 05:26:24 +0300

    found issue to be on auth repo page

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,29 +1,29 @@
-import 'package:get/get.dart';
-import 'package:turac/models/response_model.dart';
-import 'package:turac/models/signupbody.dart';
-import 'package:turac/repository/auth_repo.dart';
+// import 'package:get/get.dart';
+// import 'package:turac/models/response_model.dart';
+// import 'package:turac/models/signupbody.dart';
+// import 'package:turac/repository/auth_repo.dart';
 
 
-class Authcontroller extends GetxController implements GetxService {
-  final AuthRepo authRepo;
+// class Authcontroller extends GetxController implements GetxService {
+//   final AuthRepo authRepo;
 
-  Authcontroller({required this.authRepo});
+//   Authcontroller({required this.authRepo});
 
-  bool _isLoading = false;
-  bool get isLoading => _isLoading;
+//   bool _isLoading = false;
+//   bool get isLoading => _isLoading;
 
-  Future <ResponseModel>registration(SignUpBody signUpBody) async {
-    _isLoading = true;
-    Response response = await authRepo.registration(signUpBody);
-    late ResponseModel responseModel;
-    if (response.statusCode == 200) {
-      authRepo.saveUserToken(response.body["token"]);
-      responseModel = ResponseModel(true, response.body["token"]);
-    } else {
-      responseModel = ResponseModel(false, response.statusText!);
-    }
-    _isLoading = true;
-    update();
-    return responseModel;
-  }
-}
+//   Future <ResponseModel>registration(SignUpBody signUpBody) async {
+//     _isLoading = true;
+//     Response response = await authRepo.registration(signUpBody);
+//     late ResponseModel responseModel;
+//     if (response.statusCode == 200) {
+//       authRepo.saveUserToken(response.body["token"]);
+//       responseModel = ResponseModel(true, response.body["token"]);
+//     } else {
+//       responseModel = ResponseModel(false, response.statusText!);
+//     }
+//     _isLoading = true;
+//     update();
+//     return responseModel;
+//   }
+// }
--- lib/repository/auth_repo.dart
+++ lib/repository/auth_repo.dart
@@ -1,26 +1,26 @@
-// ignore_for_file: import_of_legacy_library_into_null_safe
+// // ignore_for_file: import_of_legacy_library_into_null_safe
 
-import 'package:get/get_connect.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:turac/data/api/api_client.dart';
-import 'package:turac/models/signupbody.dart';
-import 'package:turac/utils/app_constants.dart';
+// import 'package:get/get_connect.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:turac/data/api/api_client.dart';
+// import 'package:turac/models/signupbody.dart';
+// import 'package:turac/utils/app_constants.dart';
 
-class AuthRepo {
-  final ApiClient apiClient;
-  final SharedPreferences sharedPreferences;
+// class AuthRepo {
+//   final ApiClient apiClient;
+//   final SharedPreferences sharedPreferences;
 
-  AuthRepo({required this.apiClient, required this.sharedPreferences});
+//   AuthRepo({required this.apiClient, required this.sharedPreferences});
 
-  Future<Response> registration(SignUpBody signUpBody) async {
-    return await apiClient.postData(
-        AppConstants.register_URI, signUpBody.toJson());
-  }
+//   Future<Response> registration(SignUpBody signUpBody) async {
+//     return await apiClient.postData(
+//         AppConstants.register_URI, signUpBody.toJson());
+//   }
 
-  saveUserToken(String token) async{
-    apiClient.token = token;
-    apiClient.updateHeader(token);
-    return await sharedPreferences.setString(AppConstants.TOKEN, token);
+//   saveUserToken(String token) async{
+//     apiClient.token = token;
+//     apiClient.updateHeader(token);
+//     return await sharedPreferences.setString(AppConstants.TOKEN, token);
 
-  }
-}
+//   }
+// }
