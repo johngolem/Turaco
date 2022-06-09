01aab287178cfeaf0d172f0437cc6b75f824b6b2
John Paul <johnpaul@trees.local>
2022-06-09 13:27:51 +0300

    used default model after this commit

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -2,38 +2,42 @@
 
 
 
-class Category {
+class CategoryModel {
   String? _category;
   String? _image;
   String? _description;
 
-late List <CategoryModel> _categories;
-List <CategoryModel> get categories=>_categories;
+  late List<CategoryModel> _categories;
+  // List<CategoryModel> get categories => _categories;
 
-  Category({ required category, required image, required description}){
-     this._category = category;
-     this._image=image;
-     this._description=description;
+  CategoryModel({required category, required image, required description}) {
+    this._category = category;
+    this._image = image;
+    this._description = description;
   }
 
-   Category.fromJson(Map<String, dynamic> json) {
+  CategoryModel.fromJson(Map<String, dynamic> json) {
     _category = json['name'];
     _image = json['image_path'];
     _description = json['description'];
 
+     if (json['CategoryModel'] != null) {
+      _categories = <CategoryModel>[];
+      json['CategoryModel'].forEach((v)){
+        _categories!.add(CategoryModel.fromJson((v)));
+      };
+    }
   }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
+  // Map<String, dynamic> toJson() {
+  //   final Map<String, dynamic> data = new Map<String, dynamic>();
 
-    data['name'] = this._category;
-    data['image_path'] = this._image;
-    data['description'] = this._description;
-    return data;
-  }
+  //   data['name'] = this._category;
+  //   data['image_path'] = this._image;
+  //   data['description'] = this._description;
 
- 
+   
+  // }
 }
 
-class CategoryModel {
-}
+class CategoryModel {}
