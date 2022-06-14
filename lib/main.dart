0077a816660102decbb9b12277f7f90bd3a72c24
John Paul <johnpaul@trees.local>
2022-06-14 11:39:02 +0300

    used default model after this commit

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
       ),
      
     
-      home:MainCategoryPage(),
+      // home:MainCategoryPage(),
       // home: PopularProductDetail(),
       // home:LoginPage(),
       // home:SignUpPage(),
